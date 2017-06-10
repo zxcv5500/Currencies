4c8da79b05d1b279953cb0d786d92892145ed344
sharpen <zxcv5500@gmail.com>
2017-06-10 19:32:55 +0900

    ===== Currencies 프로젝트 : Part 2 시작 ===== - 레이아웃 id 누락 추가 - MainActivity 필드변수 추가 및 참조 지정

--- app/src/main/java/tistory/zxcv5500/currencies/MainActivity.java
+++ app/src/main/java/tistory/zxcv5500/currencies/MainActivity.java
@@ -1,13 +1,28 @@
 package tistory.zxcv5500.currencies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+	private Button mCalcButton;
+	private TextView mConvertedTextView;
+	private EditText mAmountEditText;
+	private Spinner mForSpinner, mHomSpinner;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mConvertedTextView = (TextView) findViewById(R.id.txt_converted);
+		mAmountEditText = (EditText) findViewById(R.id.edt_amount);
+		mCalcButton = (Button) findViewById(R.id.btn_calc);
+		mForSpinner = (Spinner) findViewById(R.id.spn_for);
+		mHomSpinner = (Spinner) findViewById(R.id.spn_hom);
 	}
 }
