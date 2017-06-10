90783c73050aaadc386f4a6e4b05df6e087ac429
sharpen <zxcv5500@gmail.com>
2017-06-10 17:19:25 +0900

    SplashActivitity 내에 AsyncTask 구현 클래스 추가.

--- app/src/main/java/tistory/zxcv5500/currencies/SplashActivity.java
+++ app/src/main/java/tistory/zxcv5500/currencies/SplashActivity.java
@@ -1,8 +1,11 @@
 package tistory.zxcv5500.currencies;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Window;
+
+import org.json.JSONObject;
 
 public class SplashActivity extends Activity {
 
@@ -12,5 +15,18 @@ public class SplashActivity extends Activity {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_splash);
 
+	}
+
+	private class FetchCodesTask extends AsyncTask<String, Void, JSONObject> {
+
+		@Override
+		protected void onPostExecute(JSONObject jsonObject) {
+			super.onPostExecute(jsonObject);
+		}
+
+		@Override
+		protected JSONObject doInBackground(String... params) {
+			return null;
+		}
 	}
 }
