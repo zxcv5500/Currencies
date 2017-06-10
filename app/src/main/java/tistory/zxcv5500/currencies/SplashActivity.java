0268f2b608593c7bd632cf65cea192838789d131
sharpen <zxcv5500@gmail.com>
2017-06-10 18:21:14 +0900

    통화 코드 가져오기 소스 추가.

--- app/src/main/java/tistory/zxcv5500/currencies/SplashActivity.java
+++ app/src/main/java/tistory/zxcv5500/currencies/SplashActivity.java
@@ -4,29 +4,63 @@ import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class SplashActivity extends Activity {
+	// 우리 앱에서 사용되는 통화 코드를 가져올 url
+	public static final String URL_CODES = "http://openexchangerates.org/api/currencies.json";
+	// 가져온 통화 코드를 저장하고 MainActivity에 전달할 ArrayList
+	private ArrayList<String> mCurrencies;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_splash);
+		new FetchCodesTask().execute(URL_CODES);
 
 	}
 
 	private class FetchCodesTask extends AsyncTask<String, Void, JSONObject> {
 
+		public FetchCodesTask() {
+			super();
+		}
+
 		@Override
 		protected void onPostExecute(JSONObject jsonObject) {
 			super.onPostExecute(jsonObject);
+
+			try {
+				if (jsonObject == null) {
+					throw new JSONException("no data avialable.");
+				}
+
+				Iterator iterator = jsonObject.keys();
+				String key = "";
+				mCurrencies = new ArrayList<>();
+				while (iterator.hasNext()) {
+					key = (String) iterator.next();
+					mCurrencies.add(key + " | " + jsonObject.getString(key));
+				}
+
+				finish();
+			} catch (JSONException e) {
+				Toast.makeText(SplashActivity.this, "There's Been a JSON exception: " + e.getMessage(), Toast.LENGTH_LONG).show();
+				e.printStackTrace();
+				finish();
+			}
 		}
 
 		@Override
 		protected JSONObject doInBackground(String... params) {
-			return null;
+			return new JSONParser().getJSONFromUrl(params[0]);
 		}
 	}
 }
