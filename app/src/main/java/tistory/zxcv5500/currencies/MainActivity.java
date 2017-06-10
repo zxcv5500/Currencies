043e78fca9d456d26c540d00a33eca8f541f952a
sharpen <zxcv5500@gmail.com>
2017-06-10 19:41:05 +0900

    ArrayList에 저장된 통화 코드를 배열로 저장하기

--- app/src/main/java/tistory/zxcv5500/currencies/MainActivity.java
+++ app/src/main/java/tistory/zxcv5500/currencies/MainActivity.java
@@ -7,17 +7,26 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
 
 	private Button mCalcButton;
 	private TextView mConvertedTextView;
 	private EditText mAmountEditText;
 	private Spinner mForSpinner, mHomSpinner;
+	private String[] mCurrencies;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Bundle 객체의 ArrayList 데이터를 배열로 생성한다
+		ArrayList<String> arrayList = ((ArrayList<String>) getIntent().getSerializableExtra(SplashActivity.KEY_ARRAYLIST));
+		Collections.sort(arrayList);
+		mCurrencies = arrayList.toArray(new String[arrayList.size()]);
 
 		mConvertedTextView = (TextView) findViewById(R.id.txt_converted);
 		mAmountEditText = (EditText) findViewById(R.id.edt_amount);
