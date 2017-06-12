49a3b6264320e68e8cdc2c20c281592760078688
sharpen <zxcv5500@gmail.com>
2017-06-12 18:23:31 +0900

    - MainActivity 테스트 클래스 추가 - setUp, tearDown 메소드 작성 - setUp 메소드에 테스트할 변수들의 참조 값 초기화

---
+++ app/src/androidTest/java/tistory/zxcv5500/currencies/MainActivityTest.java
@@ -0,0 +1,47 @@
+package tistory.zxcv5500.currencies;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+/**
+ * Created by sharpen on 2017. 6. 12..
+ */
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+	private MainActivity mActivity;
+	private Button mCalcButton;
+	private TextView mConvertedTextView;
+	private EditText mAmountEditText;
+	private Spinner mForSpinner, mHomSpinner;
+
+	public MainActivityTest() {
+		super(MainActivity.class);
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		// ** 테스트에 필요한 리소스에 연결하고 인텐트에 데이터(Bundle)객체에 전달하며 테스트할 액티비티와 그것의 UI 객체 참조를 얻는다
+
+		// 테스트할 액티비티 참조를 얻는다
+		mActivity = getActivity();
+
+		// 각 뷰 객체 참조를 알아내어 멤버 변수에 보존한다
+		mCalcButton = (Button) mActivity.findViewById(R.id.btn_calc);
+		mConvertedTextView = (TextView) mActivity.findViewById(R.id.txt_converted);
+		mAmountEditText = (EditText) mActivity.findViewById(R.id.edt_amount);
+		mForSpinner = (Spinner) mActivity.findViewById(R.id.spn_for);
+		mHomSpinner = (Spinner) mActivity.findViewById(R.id.spn_hom);
+
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+		// 테스트가 끝난 후 모든 리소스를 해제하는데 사용할 수 있다
+	}
+}
