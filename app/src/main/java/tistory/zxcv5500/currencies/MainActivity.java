993d46ed4a5f3718ab550ec63abf4ee1c2b51d06
sharpen <zxcv5500@gmail.com>
2017-06-12 16:27:46 +0900

    CurrencyConverterTask 내부 클래스 추가

--- app/src/main/java/tistory/zxcv5500/currencies/MainActivity.java
+++ app/src/main/java/tistory/zxcv5500/currencies/MainActivity.java
@@ -1,11 +1,14 @@
 package tistory.zxcv5500.currencies;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -17,6 +20,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -226,5 +233,65 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 	@Override
 	public void onNothingSelected(AdapterView<?> parent) {
 
+	}
+
+	private class CurrencyConverterTask extends AsyncTask<String, Void, JSONObject> {
+
+		private ProgressDialog progressDialog;
+
+		@Override
+		protected void onPreExecute() {
+
+			progressDialog = new ProgressDialog(MainActivity.this);
+			progressDialog.setTitle("Calculating Result...");
+			progressDialog.setMessage("One moment please...");
+			progressDialog.setCancelable(true);
+
+			progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE,
+					"Cancel", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							CurrencyConverterTask.this.cancel(true);
+							progressDialog.dismiss();
+						}
+					});
+
+			progressDialog.show();
+		}
+
+		@Override
+		protected void onPostExecute(JSONObject jsonObject) {
+			double dCalculated = 0.0;
+			String strForCode =
+					extractCodeFromCurrency(mCurrencies[mForSpinner.getSelectedItemPosition()]);
+			String strHomCode = extractCodeFromCurrency(mCurrencies[mHomSpinner.getSelectedItemPosition()]);
+			String strAmount = mAmountEditText.getText().toString();
+
+			try {
+				if (jsonObject == null) {
+					throw new JSONException("no data available");
+				}
+
+				JSONObject jsonRates = jsonObject.getJSONObject(RATES);
+				if (strHomCode.equalsIgnoreCase("USD")) {
+					dCalculated = Double.parseDouble(strAmount) / jsonRates.getDouble(strForCode);
+				} else if (strForCode.equalsIgnoreCase("USD")) {
+					dCalculated = Double.parseDouble(strAmount) * jsonRates.getDouble(strHomCode);
+				} else {
+					dCalculated = Double.parseDouble(strAmount) * jsonRates.getDouble(strHomCode) / jsonRates.getDouble(strForCode);
+				}
+			} catch (JSONException e) {
+				Toast.makeText(MainActivity.this, "There's been a JSON exception : " + e.getMessage(), Toast.LENGTH_SHORT).show();
+				mConvertedTextView.setText("");
+				e.printStackTrace();
+			}
+			mConvertedTextView.setText(DECIMAL_FORMAT.format(dCalculated) + " " + strHomCode);
+			progressDialog.dismiss();
+		}
+
+		@Override
+		protected JSONObject doInBackground(String... params) {
+			return new JSONParser().getJSONFromUrl(params[0]);
+		}
 	}
 }
